fefb2d113e5cab932bad60be56b8f3b4d4cc2bec
FranciscoMG <franciscojavier.melendez@ucr.ac.cr>
2017-09-23 15:32:38 -0600

    Plantilla de correo cambiada

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword as ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Sobreescribir el metodo de envio de recuperación.
+     *
+     */
+    public function sendPasswordResetNotification($token)
+    {
+       $this->notify(new ResetPasswordNotification($token));
+    }
 }
